9823f1b2d1f0285b8a3f4ba15c94b6aebbda8a74
ashwanikd0123 <ashwanikd0123@gmail.com>
2026-09-02 22:19:17 +0530

    unused import

--- app/src/main/java/com/example/fluidvectorar/data/local/entity/LayerEntity.kt
+++ app/src/main/java/com/example/fluidvectorar/data/local/entity/LayerEntity.kt
@@ -4,8 +4,6 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.Index
 import androidx.room.PrimaryKey
-import com.example.fluidvectorar.domain.model.LayerState
-import com.example.fluidvectorar.domain.model.StrokeData
 import java.util.UUID
 
 
